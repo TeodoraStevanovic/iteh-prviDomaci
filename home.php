826eab3d20d276b1f9303d777400af6dabd1f00b
Teodora Stevanovic <teodorastevanovic26@gmail.com>
2022-01-27 20:23:54 +0100

    Inicijalizovan kostur projekta

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/druga.css">
+    <title>CineStar</title>
+</head>
+<body>
+<h1 id="naslov">CineStar-informacije o projekcijama</h1>  
+<div class="zaglavlje">
+
+      <ul class="pozadina">
+        <li><button id="btn-prikazi">Prikazi sve projekcije</button></li>
+        <li><button id="btn-pretraga">Pretrazi projekcije</button></li>
+        <li><button id="btn-zakazivanje">Zakazi novu projekciju</button></li>
+        <li><button id="btn-prikazi">Obrisi sve projekcije</button></li>
+       
+      
+         </ul>
+
+
+</div>
+    
+</body>
+</html>
