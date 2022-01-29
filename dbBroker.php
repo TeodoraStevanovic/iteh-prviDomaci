3eb9a4646de497fa5f8cae404778030bb5792c5a
Teodora Stevanovic <teodorastevanovic26@gmail.com>
2022-01-29 02:01:21 +0100

    kreiran dbBroker

--- dbBroker.php
+++ dbBroker.php
@@ -1,12 +1,13 @@
 <?php
 $host = "localhost";
+//naziv baze 
 $db = "domaci1";
 $user = "root";
 $pass = "";
-
+//kreiramo konekciju
 $conn = new mysqli($host,$user,$pass,$db);
 
-
+//ako se desi greska sa konekcijom hocemo da nam ispise sledece
 if ($conn->connect_errno){
     exit("Nauspesna konekcija: greska> ".$conn->connect_error.", err kod>".$conn->connect_errno);
 }
